77163811b9ded60589d103882346a05b5caf3bb9
nullfullme <world_view_error@163.com>
2023-09-08 22:42:24 +0800

    first commit

---
+++ test.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001
+@echo 开始运行
+set PYTHON=python\python.exe
+set COMMANDLINE_ARGS=--xformers --opt-channelslast --api --autolaunch --listen --port 7789
+set TRANSFORMERS_CACHE=.cache
+python\python.exe launch.py
+@echo 处理完毕
+pause
